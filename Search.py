a2e9cb688a2eb59a6b5660ce33a45285a6c43b72
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-10-09 14:56:16 +0200

    :construction:

--- Search.py
+++ Search.py
@@ -1,55 +1,71 @@
-import datetime
 import os
+import datetime
 
 
-def list_files_by_extension(folder_path):
-    print(f"Searching in: {folder_path}")
-    files = {}
-    for root, _, filenames in os.walk(folder_path):
-        print(f"Root: {root}")
-        for filename in filenames:
-            print(f"\t{filename}")
-            file_path = os.path.join(root, filename)
-            extension = os.path.splitext(filename)[1]
-            if extension not in files:
-                files[extension] = []
-            files[extension].append(file_path)
+def list_files(path):
+    """
+    Renvoie une liste de tous les fichiers dans un répertoire, y compris les fichiers des sous-répertoires.
+
+    Args:
+      path: Le chemin du répertoire à lister.
+
+    Returns:
+      Une liste de chemins de fichiers.
+    """
+
+    files = []
+    for root, dirs, _ in os.walk(path):
+        for file in dirs:
+            files.append(os.path.join(root, file))
+        for file in os.listdir(root):
+            files.append(os.path.join(root, file))
     return files
 
 
-def print_files_by_extension(files):
-    print("Files by extension:")
-    for extension, file_paths in sorted(files.items()):
-        print(f"Extension: {extension}")
-        for file_path in sorted(file_paths):
-            parent_folder = os.path.dirname(file_path)
-            print(f"\t{parent_folder}: {file_path}")
-    return ""
+def get_file_info(file):
+    """
+    Renvoie des informations sur un fichier, notamment son extension, son répertoire parent et son nom.
+
+    Args:
+      file: Le chemin du fichier.
+
+    Returns:
+      Un dictionnaire contenant les informations suivantes :
+        * extension: L'extension du fichier, sans le point.
+        * parent: Le répertoire parent du fichier.
+        * name: Le nom du fichier.
+    """
+
+    extension = os.path.splitext(file)[1]
+    parent = os.path.dirname(file)
+    name = os.path.basename(file)
+    return {
+        "extension": extension,
+        "parent": parent,
+        "name": name,
+    }
 
 
-def save_output_to_file(output, filename):
-    if output is None:
-        output = ""
+def main():
+    """
+    Exécute le programme.
+    """
+
+    # Obtenir la date et l'heure actuelles.
+    now = datetime.datetime.now()
+
+    # Créer le nom du fichier de sortie.
+    filename = "SearchFile" + "-" + now.strftime("%Y-%m-%d-%H-%M-%S")
+
+    # Lister tous les fichiers dans le répertoire courant.
+    files = list_files(".")
+
+    # Enregistrer les informations sur les fichiers dans un fichier.
     with open(filename, "w") as f:
-        f.write(output)
+        for file in files:
+            info = get_file_info(file)
+            f.write(f"{info['extension']}\t{info['parent']}\t{info['name']}\n")
 
 
 if __name__ == "__main__":
-    folder_path = os.path.dirname(__file__)
-
-    files = list_files_by_extension(folder_path)
-
-    # Check if the `files` dictionary is empty.
-    if not files:
-        print("No files found in the directory.")
-        exit()
-
-    # Print the output of the `print_files_by_extension()` function to the console.
-    output = print_files_by_extension(files)
-    print(output)
-
-    # Save the output to a file.
-    now = datetime.datetime.now()
-    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"Search-File[{date_time}].txt"
-    save_output_to_file(output, filename)
+    main()
