c6e1a4f4a1fcb343a8eb026ca8d4aa4e7cb486c8
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-10-09 15:53:07 +0200

    Update Application.py
    Menu

--- Application.py
+++ Application.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 from program import *
 
 
@@ -24,12 +25,31 @@ def add_programs_to_path():
 
 
 def menu():
-    # print(f"{len(programs)} program(s) available")
+    """Display the menu and allow the user to launch a program."""
+
+    global programs
+
+    # Print the menu options
     print("=== Menu ===")
     for i in range(len(programs)):
         print(f"{i + 1} - {programs[i]}")
 
+    # Get the user's choice
+    choice = int(input("Enter your choice: "))
 
-add_programs_to_path()
-clear_console()
-menu()
+    # Launch the selected program
+    if 1 <= choice <= len(programs):
+        program_name = programs[choice - 1]
+        program_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "program", f"{program_name}.py"
+        )
+        exec(open(program_path, "r").read())
+        sys.exit()
+    else:
+        print("Invalid choice.")
+
+
+if __name__ == "__main__":
+    add_programs_to_path()
+    clear_console()
+    menu()
