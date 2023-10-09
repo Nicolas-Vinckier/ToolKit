1862de9840f24c8e2f9b783361f2f52eb7bbf767
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-10-09 15:42:25 +0200

    Menu

---
+++ Application.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from program import *
+
+
+# liste tous les programmes dans le dossier "program" (du répertoire courant)
+def add_programs_to_path():
+    path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(path, "program")
+    sys.path.append(path)
+
+    global programs
+    programs = []
+
+    for filename in os.listdir(path):
+        filepath = os.path.join(path, filename)
+        if os.path.isfile(filepath) and filename.endswith(".py"):
+            programs.append(filename[:-3])
+
+    return programs
+
+
+def menu():
+    # print(f"{len(programs)} program(s) available")
+    print("=== Menu ===")
+    for i in range(len(programs)):
+        print(f"{i + 1} - {programs[i]}")
+
+
+add_programs_to_path()
+menu()
